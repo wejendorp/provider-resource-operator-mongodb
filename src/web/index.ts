0f25a6995f3c67fa40a1f1cda36a39f250a941a8
Henrik Hofmeister <henrik@newdawn.dk>
2022-12-30 17:31:33 +0100

    Added support for versions (#1)
    * Added support for versions
    
    * Publish to internal NPM

--- src/web/index.ts
+++ src/web/index.ts
@@ -8,9 +8,11 @@ import {
 } from '@blockware/ui-web-types';
 
 const definition = require('../../blockware.yml');
+const packageJson = require('../../package.json');
 
 const config: ResourceConfig<ResourceMetadata> = {
     kind: definition.metadata.name,
+    version: packageJson.version,
     title: definition.metadata.title,
     role: ResourceRole.CONSUMES,
     type: ResourceType.DATABASE,
