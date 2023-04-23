fe2dab90705bbde583fe6e6d7c2f0d609cd2f354
Henrik Hofmeister <henrik@newdawn.dk>
2023-04-23 17:42:19 +0200

    Update interface

--- src/web/index.ts
+++ src/web/index.ts
@@ -16,7 +16,7 @@ const resourceTypeProvider: IResourceTypeProvider<Metadata> = {
     title: definition.metadata.title,
     role: ResourceRole.CONSUMES,
     type: ResourceProviderType.OPERATOR,
-    componentType: MongoDBEditorComponent,
+    editorComponent: MongoDBEditorComponent,
     definition
 };
 
