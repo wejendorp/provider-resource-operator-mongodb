ee9d838782e0473310d5e6894bdd8cc1a09e0eb2
Henrik Hofmeister <henrik@newdawn.dk>
2023-04-23 17:12:33 +0200

    Bump deps and fix build

--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,8 @@ module.exports = {
             }
         ]
     },
+    devtool: process.env.NODE_ENV === 'production' ?
+        'source-map' : 'inline-source-map',
     resolve: {
         extensions: [
             '.js',
